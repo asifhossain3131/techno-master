2508d38ca8a8cbcc72ce66e2431f52bf094066e0
asifhossain3131 <asifhossain3131@gmail.com>
2023-08-12 22:35:25 +0600

    private route added

--- src/app/(common)/page.jsx
+++ src/app/(common)/page.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+'use client'
+import { useEffect, useState } from 'react';
 import HomeSlider from '../components/home components/HomeSlider';
+import { useRouter } from 'next/navigation';
 
 const WithCommon = () => {
+    const router=useRouter()
+ useEffect(()=>{
+    const user=localStorage.getItem('userInformation')
+    if(!user){
+        return router.push('/userAuth/login')
+    }
+    else{
+        return router.push('/')
+    }
+ },[router])
     return (
         <div>
             <HomeSlider></HomeSlider>
